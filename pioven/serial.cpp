c04b52777a0151f5cdf974fcb5b14b5c61a01da4
PrincessSparkles <Princess@dreamsilver.local>
2015-09-05 20:32:26 +0100

    Reading response from 'v' command works!

--- pioven/serial.cpp
+++ pioven/serial.cpp
@@ -9,6 +9,7 @@
 /* ************************************************************************* */
 
 #include <ntddk.h>
+#include <ntddser.h>
 
 #include "pioven.h"
 #include "pioven-driver.h"
@@ -17,9 +18,99 @@
 
 /* ************************************************************************* */
 /* ************************************************************************* */
+/* ************************************************************************* */
 
-NTSTATUS	ConfigureSerialPort(HANDLE /*hComPort*/)
+NTSTATUS DoSerialPortOutIOCTL(HANDLE hComPort, ULONG ioctl, PVOID outBuffer, ULONG outBufferSize)
 {
+	IO_STATUS_BLOCK ioStatusBlock;
+
+	NTSTATUS status = ZwDeviceIoControlFile(hComPort, NULL, NULL, NULL, &ioStatusBlock, ioctl, NULL, 0, outBuffer, outBufferSize);
+
+	return status;
+}
+
+/* ************************************************************************* */
+
+NTSTATUS DoSerialPortIOCTL(HANDLE hComPort, ULONG ioctl, PVOID inBuffer, ULONG inBufferSize)
+{
+	IO_STATUS_BLOCK ioStatusBlock;
+
+	NTSTATUS status = ZwDeviceIoControlFile(hComPort, NULL, NULL, NULL, &ioStatusBlock, ioctl, inBuffer, inBufferSize, NULL, 0);
+
+	return status;
+}
+
+/* ************************************************************************* */
+
+NTSTATUS	ConfigureSerialPort(HANDLE hComPort)
+{
+	//#define CTL_CODE( DeviceType, Function, Method, Access ) (                 \
+	//    ((DeviceType) << 16) | ((Access) << 14) | ((Function) << 2) | (Method) \
+	//)
+	//	ULONG foo = IOCTL_SERIAL_SET_BAUD_RATE;
+	//	// 9600
+	//
+	//	foo = IOCTL_SERIAL_SET_LINE_CONTROL;
+	//	// 00 00 08
+	//
+	//	foo = IOCTL_SERIAL_SET_CHARS;
+	//	// 00 00 00 00 11 13
+	//
+	//	foo = IOCTL_SERIAL_SET_HANDFLOW;
+	//	// 01 00 00 00  43 00 00 00  00 08 00 00  00 02 00 00
+	//
+	//	foo = IOCTL_SERIAL_SET_TIMEOUTS;
+	//	// 01 00 .. 00
+
+	// these values were taken from running PUTTY and capturing
+	// NtDeviceIoControlFile with API Monitor
+	SERIAL_BAUD_RATE baudRate;
+	SERIAL_LINE_CONTROL	lineControl;
+	SERIAL_CHARS	chars;
+	SERIAL_HANDFLOW handFlow;
+
+	DoSerialPortOutIOCTL(hComPort, IOCTL_SERIAL_GET_BAUD_RATE, &baudRate, sizeof(SERIAL_BAUD_RATE));
+	DoSerialPortOutIOCTL(hComPort, IOCTL_SERIAL_GET_LINE_CONTROL, &lineControl, sizeof(SERIAL_LINE_CONTROL));
+	DoSerialPortOutIOCTL(hComPort, IOCTL_SERIAL_GET_CHARS, &chars, sizeof(SERIAL_CHARS));
+	DoSerialPortOutIOCTL(hComPort, IOCTL_SERIAL_GET_HANDFLOW, &handFlow, sizeof(SERIAL_HANDFLOW));
+
+	DoSerialPortOutIOCTL(hComPort, IOCTL_SERIAL_GET_BAUD_RATE, &baudRate, sizeof(SERIAL_BAUD_RATE));
+	DoSerialPortOutIOCTL(hComPort, IOCTL_SERIAL_GET_LINE_CONTROL, &lineControl, sizeof(SERIAL_LINE_CONTROL));
+	DoSerialPortOutIOCTL(hComPort, IOCTL_SERIAL_GET_CHARS, &chars, sizeof(SERIAL_CHARS));
+	DoSerialPortOutIOCTL(hComPort, IOCTL_SERIAL_GET_HANDFLOW, &handFlow, sizeof(SERIAL_HANDFLOW));
+
+	baudRate.BaudRate = 9600;
+	DoSerialPortIOCTL(hComPort, IOCTL_SERIAL_SET_BAUD_RATE, &baudRate, sizeof(SERIAL_BAUD_RATE));
+
+	DoSerialPortIOCTL(hComPort, IOCTL_SERIAL_SET_RTS, NULL, 0);
+	DoSerialPortIOCTL(hComPort, IOCTL_SERIAL_SET_DTR, NULL, 0);
+
+	lineControl.Parity = 0;
+	lineControl.StopBits = 0;
+	lineControl.WordLength = 8;
+	DoSerialPortIOCTL(hComPort, IOCTL_SERIAL_SET_LINE_CONTROL, &lineControl, sizeof(SERIAL_LINE_CONTROL));
+
+	chars.EofChar = 0;
+	chars.ErrorChar = 0;
+	chars.BreakChar = 0;
+	chars.EventChar = 0;
+	chars.XonChar = 0x11;
+	chars.XoffChar = 0x13;
+	DoSerialPortIOCTL(hComPort, IOCTL_SERIAL_SET_CHARS, &chars, sizeof(SERIAL_CHARS));
+
+	handFlow.ControlHandShake = SERIAL_DTR_CONTROL;
+	handFlow.FlowReplace = SERIAL_AUTO_TRANSMIT | SERIAL_AUTO_RECEIVE | SERIAL_RTS_CONTROL;
+	handFlow.XonLimit = 0x800;
+	handFlow.XoffLimit = 0x200;
+
+	SERIAL_TIMEOUTS timeouts;
+	timeouts.ReadIntervalTimeout = 2000;
+	timeouts.ReadTotalTimeoutMultiplier = 0;
+	timeouts.ReadTotalTimeoutConstant = 0;
+	timeouts.WriteTotalTimeoutMultiplier = 0;
+	timeouts.WriteTotalTimeoutConstant = 0;
+	DoSerialPortIOCTL(hComPort, IOCTL_SERIAL_SET_TIMEOUTS, &timeouts, sizeof(SERIAL_TIMEOUTS));
+
 	return STATUS_SUCCESS;
 }
 
@@ -86,10 +177,10 @@ NTSTATUS	SendSerialCommand(HANDLE hComPort, CHAR cmd, CHAR *response, ULONG resp
 			*response = 0;
 			ULONG totalReceived = 0;
 
-			while (response[totalReceived] != '\n')
+			while (responseSize > 0)
 			{
 				status = ZwReadFile(hComPort, NULL, NULL, NULL, &ioStatusBlock, &response[totalReceived],
-					responseSize - totalReceived, &offset, NULL);
+					1, &offset, NULL);
 
 				if (NT_SUCCESS(status) == FALSE)
 				{
@@ -99,6 +190,12 @@ NTSTATUS	SendSerialCommand(HANDLE hComPort, CHAR cmd, CHAR *response, ULONG resp
 				else
 				{
 					totalReceived += (ULONG) ioStatusBlock.Information;
+					responseSize -= (ULONG)ioStatusBlock.Information;
+
+					if (response[totalReceived - 1] == '\n')
+					{
+						break;
+					}
 				}
 			}
 		}
